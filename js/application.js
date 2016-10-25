858d2077bf32734f9e6574ef44c513c98235ae29
Paweł Gościcki <pawel.goscicki@gmail.com>
2016-10-25 17:50:47 +0200

    Add two very simple components
    (in a parent/child relationship)

--- js/application.js
+++ js/application.js
@@ -1,3 +1,19 @@
+// Parent | Subreddit component containing a list of 'post' components.
+var subreddit = Vue.component('subreddit', {
+  template: '#subreddit',
+  props: ['name'],
+
+  data: function () {
+    return { posts: ['abc', 'cde'] }
+  }
+});
+
+// Child | Componenet represiting a single post.
+var post = Vue.component('post', {
+  template: "#post",
+  props: ['item']
+});
+
 new Vue({
   el: '#vue'
 });
